86a478103e51b233f0632c0671399ec1b840bc92
Abraxas2312 <theabrax@googlemail.com>
2014-07-11 16:09:17 +0200

    Create path.js

---
+++ www/path.js
@@ -0,0 +1,12 @@
+cordova.define("de.fb08.hsnr.path.ContentPathReceiver", function(require, exports, module) {
+	var exec = require('cordova/exec');
+	
+	var path = {
+		getContentPath: function(filePath, successCallback, errorCallback) {
+			imagesViewController.showError("getting path")
+			exec(successCallback, errorCallback, "Path", "getContentPath", [filePath])
+		}
+	};
+
+	module.exports = path;	
+});
