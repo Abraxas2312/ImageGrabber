f283f71b94a7b1aa1c1a4221dcd79eb13b1f4ffe
Abraxas2312 <theabrax@googlemail.com>
2014-07-12 02:11:01 +0200

    Update Path.java

--- src/android/Path.java
+++ src/android/Path.java
@@ -1,48 +1,69 @@
 package de.fb08.hsnr.path;
 
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
 
 public class Path extends CordovaPlugin {
+
 	@Override
 	public boolean execute(String action, JSONArray args,
 			CallbackContext callbackContext) throws JSONException {
-		if (action.equals("getContentPath")) {
-			final String filePath = args.getString(0);
-			final String contentPath = getContentPath(filePath, cordova);
-			callbackContext.sendPluginResult(new PluginResult(
-					PluginResult.Status.OK, contentPath));
+		if (action.equals("getContentPaths")) {
+			final String contentPath = getContentPaths(true, cordova
+					.getActivity().getContentResolver())
+					+ "#"
+					+ getContentPaths(false, cordova.getActivity()
+							.getContentResolver());
 			if (contentPath != null) {
+				callbackContext.sendPluginResult(new PluginResult(
+						PluginResult.Status.OK, contentPath));
 				return true;
 			}
+
 		}
 		return false;
 	}
 
-	public String getContentPath(String filePath, CordovaInterface cordova) {
-		String contentPath = null;
-		Cursor cursor = cordova
-				.getActivity()
-				.getContentResolver()
-				.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-						new String[] { MediaStore.Images.Media._ID },
-						MediaStore.Images.Media.DATA + "=? ",
-						new String[] { filePath }, null);
-		if (cursor != null && cursor.moveToFirst()) {
-			int id = cursor.getInt(cursor
-					.getColumnIndex(MediaStore.MediaColumns._ID));
-			Uri baseUri = Uri.parse("content://media/external/images/media");
-			Uri uri = Uri.withAppendedPath(baseUri, "" + id);
-			contentPath = uri.toString();
+	private String getContentPaths(final boolean internal,
+			final ContentResolver contentResolver) {
+		try {
+			final Uri imagesUri;
+			if (internal) {
+				// should be something like
+				// content://media/internal/images/media/
+				imagesUri = MediaStore.Images.Media.getContentUri("internal");
+			} else {
+				// should be something like
+				// content://media/external/images/media/
+				imagesUri = MediaStore.Images.Media.getContentUri("external");
+			}
+			// ID column
+			final String[] projection = { MediaStore.Images.ImageColumns._ID };
+			// Get all images ids from the library
+			final Cursor cursor = contentResolver.query(imagesUri, projection,
+					null, null, null);
+			final StringBuilder stringBuilder = new StringBuilder("");
+			cursor.moveToFirst();
+			for (int i = 0, len = cursor.getCount(); i < len; i++) {
+				stringBuilder.append(imagesUri.toString() + "/"
+						+ cursor.getLong(0) + "#");
+				cursor.moveToNext();
+			}
+			// clean up last #
+			stringBuilder.deleteCharAt(stringBuilder.lastIndexOf("#"));
+			cursor.close();
+			return stringBuilder.toString();
+		} catch (final Exception e) {
+			return null;
 		}
-		return contentPath;
 	}
+
 }
