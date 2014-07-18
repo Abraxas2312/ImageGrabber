2103a0aa6f6abbbccbd56ba0fceb7ed31b6cae5c
Abraxas2312 <theabrax@googlemail.com>
2014-07-18 09:33:50 +0200

    Update ImageGrabber.java

--- src/android/ImageGrabber.java
+++ src/android/ImageGrabber.java
@@ -1,5 +1,7 @@
-package de.fb08.hsnr.path;
+package de.fb08.hsnr.imageGrabber;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,49 +19,67 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
 
+// TODO: Code should be optimized
 public class ImageGrabber extends CordovaPlugin {
-
 	@Override
 	public boolean execute(String action, JSONArray args,
-			CallbackContext callbackContext) throws JSONException {
-		if (action.equals("getImages")) {
-			final JSONArray images = new JSONArray();
-			getImages(true, cordova.getActivity().getContentResolver(),
-					images);
-			getImages(false, cordova.getActivity().getContentResolver(),
-					images);
-			callbackContext.sendPluginResult(new PluginResult(
-					PluginResult.Status.OK, images));
+			CallbackContext callbackContext) {
+		try {
+			if (action.equals("getImages")) {
+				final JSONArray images = new JSONArray();
+				final ContentResolver contentResolver = cordova.getActivity()
+						.getContentResolver();
+				getImages(true, contentResolver, images);
+				getImages(false, contentResolver, images);
+				callbackContext.sendPluginResult(new PluginResult(
+						PluginResult.Status.OK, images));
+				return true;
+			} else if (action.equals("getImageData")) {
+				final String imageUri = args.getString(0);
+				final ContentResolver contentResolver = cordova.getActivity()
+						.getContentResolver();
+				JSONObject image = getImageData(true, contentResolver, imageUri);
+				if (image == null) {
+					image = getImageData(false, contentResolver, imageUri);
+				}
+				if (image == null) {
+					callbackContext.sendPluginResult(new PluginResult(
+							PluginResult.Status.ERROR, "No Data found"));
+				} else {
+					callbackContext.sendPluginResult(new PluginResult(
+							PluginResult.Status.OK, image));
+				}
+				return true;
+			} else {
+				return false;
+			}
+		} catch (final Exception e) {
+			return false;
 		}
-		return false;
 	}
 
 	private void getImages(final boolean internal,
 			final ContentResolver contentResolver, final JSONArray images)
-			throws JSONException {
+			throws JSONException, FileNotFoundException, IOException {
 		final Uri imagesUri;
 		final Uri thumbnailsUri;
 		if (internal) {
 			// content://media/internal/images/media/
-			imagesUri = MediaStore.Images.Media.getContentUri("internal");
+			imagesUri = MediaStore.Images.Media.INTERNAL_CONTENT_URI;
 			// content://media/internal/images/thumbnails/
-			thumbnailsUri = MediaStore.Images.Thumbnails
-					.getContentUri("internal");
+			thumbnailsUri = MediaStore.Images.Thumbnails.INTERNAL_CONTENT_URI;
 		} else {
 			// content://media/external/images/media/
-			imagesUri = MediaStore.Images.Media.getContentUri("external");
+			imagesUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 			// content://media/external/images/thumbnails/
-			thumbnailsUri = MediaStore.Images.Thumbnails
-					.getContentUri("external");
+			thumbnailsUri = MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI;
 		}
 		// Columns for images
 		final String[] imagesProjection = { MediaStore.Images.ImageColumns._ID,
-				MediaStore.Images.ImageColumns.DATA,
 				MediaStore.Images.ImageColumns.TITLE,
 				MediaStore.Images.ImageColumns.LATITUDE,
 				MediaStore.Images.ImageColumns.LONGITUDE,
-				MediaStore.Images.ImageColumns.DATE_TAKEN,
-				MediaStore.Images.ImageColumns.ORIENTATION };
+				MediaStore.Images.ImageColumns.DATE_TAKEN };
 		// Images query
 		final Cursor imagesCursor = contentResolver.query(imagesUri,
 				imagesProjection, null, null, null);
@@ -75,15 +95,13 @@ public class ImageGrabber extends CordovaPlugin {
 		thumbnailsCursor.moveToFirst();
 		for (int i = 0, iLen = imagesCursor.getCount(); i < iLen; i++) {
 			final long imageId = imagesCursor.getLong(0);
-			final String dataPath = imagesCursor.getString(1);
-			final String title = imagesCursor.getString(2);
-			final String latitude = imagesCursor.getString(3);
-			final String longitude = imagesCursor.getString(4);
+			final String title = imagesCursor.getString(1);
+			final String latitude = imagesCursor.getString(2);
+			final String longitude = imagesCursor.getString(3);
 			final SimpleDateFormat dateFormat = new SimpleDateFormat(
 					"dd.MM.yyyy - hh:mm:ss", Locale.GERMANY);
 			final String date = dateFormat.format(new Date(new Timestamp(
-					imagesCursor.getLong(5)).getTime()));
-			final int orientation = imagesCursor.getInt(6);
+					imagesCursor.getLong(4)).getTime()));
 			Long miniId = null;
 			Long microId = null;
 			for (int j = 0, jLen = thumbnailsCursor.getCount(); j < jLen; j++) {
@@ -102,18 +120,87 @@ public class ImageGrabber extends CordovaPlugin {
 			thumbnailsCursor.moveToFirst();
 			final JSONObject image = new JSONObject();
 			image.put("id", imageId);
-			image.put("contentPath", imagesUri.toString() + imageId);
-			image.put("dataPath", dataPath);
+			image.put("contentPath", imagesUri.toString());
 			image.put("title", title);
 			image.put("latitude", latitude);
 			image.put("longitude", longitude);
 			image.put("date", date);
-			image.put("orientation", orientation);
 			image.put("miniId", miniId);
 			image.put("microId", microId);
 			image.put("thumbnailPath", thumbnailsUri.toString());
 			images.put(image);
 			imagesCursor.moveToNext();
 		}
+	}
+
+	private JSONObject getImageData(final boolean internal,
+			final ContentResolver contentResolver, final String imageUri)
+			throws JSONException {
+		final Uri imagesUri;
+		final Uri thumbnailsUri;
+		if (internal) {
+			// content://media/internal/images/media/
+			imagesUri = MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+			// content://media/internal/images/thumbnails/
+			thumbnailsUri = MediaStore.Images.Thumbnails.INTERNAL_CONTENT_URI;
+		} else {
+			// content://media/external/images/media/
+			imagesUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+			// content://media/external/images/thumbnails/
+			thumbnailsUri = MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI;
+		}
+		final String filePath = imageUri.substring("file://".length());
+		final String[] imageProjection = { MediaStore.Images.ImageColumns._ID,
+				MediaStore.Images.ImageColumns.TITLE,
+				MediaStore.Images.ImageColumns.LATITUDE,
+				MediaStore.Images.ImageColumns.LONGITUDE,
+				MediaStore.Images.ImageColumns.DATE_TAKEN };
+		Cursor cursor = contentResolver.query(imagesUri, imageProjection,
+				MediaStore.Images.Media.DATA + "=? ",
+				new String[] { filePath }, null);
+		if (cursor.getCount() <= 0) {
+			return null;
+		}
+		cursor.moveToFirst();
+		final long imageId = cursor.getLong(0);
+		final String title = cursor.getString(1);
+		final String latitude = cursor.getString(2);
+		final String longitude = cursor.getString(3);
+		final SimpleDateFormat dateFormat = new SimpleDateFormat(
+				"dd.MM.yyyy - hh:mm:ss", Locale.GERMANY);
+		final String date = dateFormat.format(new Date(new Timestamp(cursor
+				.getLong(4)).getTime()));
+		final String[] thumbnailsProjection = {
+				MediaStore.Images.Thumbnails._ID,
+				MediaStore.Images.Thumbnails.KIND };
+		cursor = contentResolver.query(thumbnailsUri, thumbnailsProjection,
+				MediaStore.Images.Thumbnails.IMAGE_ID + "=? ",
+				new String[] { String.valueOf(imageId) }, null);
+		Long miniId = null;
+		Long microId = null;
+		for (int j = 0, jLen = cursor.getCount(); j < jLen; j++) {
+			final long thumbnailImageId = cursor.getLong(1);
+			if (thumbnailImageId == imageId) {
+				final int kind = cursor.getInt(2);
+				if (kind == MediaStore.Images.Thumbnails.MINI_KIND) {
+					miniId = cursor.getLong(0);
+				} else if (kind == MediaStore.Images.Thumbnails.MICRO_KIND) {
+					microId = cursor.getLong(0);
+				}
+				break;
+			}
+			cursor.moveToNext();
+		}
+		final JSONObject image = new JSONObject();
+		image.put("id", imageId);
+		image.put("contentPath", imagesUri.toString());
+		image.put("title", title);
+		image.put("latitude", latitude);
+		image.put("longitude", longitude);
+		image.put("date", date);
+		image.put("miniId", miniId);
+		image.put("microId", microId);
+		image.put("thumbnailPath", thumbnailsUri.toString());
+		return image;
 	}
 }
