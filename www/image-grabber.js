75f8a16554ee243c86f87c3100d37bf5bff9b707
Abraxas2312 <theabrax@googlemail.com>
2014-07-18 04:15:48 +0200

    Update image-grabber.js

--- www/image-grabber.js
+++ www/image-grabber.js
@@ -2,11 +2,6 @@ var imageGrabber = {
 	getImages: function(successCallback, errorCallback) {
 		cordova.exec(successCallback, errorCallback, "ImageGrabber", "getImages", []);
 	}
-	
-	getImageData: function(imageUri, successCallback, errorCallback) {
-		cordova.exec(succesCallback, errorCallback, "ImageGrabber", "getImageData", [
-			imageUri]);
-	}
 };
 
 module.exports = imageGrabber;
